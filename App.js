19e76647dd31c357dbf912c0940057522f6cc06c
Hugendas <sohamshetty12@gmail.com>
2022-01-22 16:43:46 -0500

    hi

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Kaushal is a sexy man</Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
