69e8716e755a8e609f07c01cb4a36ae755c82e8b
kaushal380 <kaushal.sambanna@gmail.com>
2022-01-22 16:53:01 -0500

    commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
