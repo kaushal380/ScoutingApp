29a32923e2242d9a89cc54e52edeb64b8bf9d347
kaushal380 <kaushal.sambanna@gmail.com>
2022-01-23 01:11:43 -0500

    slight stuff for now:

---
+++ screens/DataCollection/DataCollect.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import React from 'react';
+import {AntDesign, Entypo} from "@expo/vector-icons"
+
+const DataCollect = ({
+    isInputVisible, setIsInputVisible, 
+    taxi, setTaxi, 
+    AutoUpper, setAutoUpper, 
+    autoLower, setAutoLower, 
+    TeleUpper, setTeleUpper, 
+    TeleLower, setTeleLower, 
+    hanger, setHanger, 
+    outcome, setOutcome,
+}) => {
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default DataCollect;
+
+const styles = StyleSheet.create({
+
+});
