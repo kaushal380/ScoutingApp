514a19a4c73fc11b411e63fb7b42ce34299ac85e
Cyrus <cyrusyong1@gmail.com>
2022-02-04 16:16:03 -0500

    added data collection components

---
+++ screens/DataCollection/CargoData.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+export class CargoData extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Cargo Collection Component</Text>
+      </View>
+    );
+  }
+}
